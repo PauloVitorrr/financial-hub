3aa3cc8bd5a6d57093d86cb160e66a37011714a7
Paulo Vitor <paulovitorvaztst@gmail.com>
2025-10-17 11:30:08 -0300

    configurações e estilos globais

---
+++ src/styles/theme/index.ts
@@ -0,0 +1,11 @@
+export const defaultTheme = {
+  white: "#fff",
+
+  "gray-800": "#202024",
+  "gray-900": "#121214",
+
+  "green-500": "#00875F",
+  "green-700": "#015F43",
+
+  "red-500": "#AB222E",
+} as const;
